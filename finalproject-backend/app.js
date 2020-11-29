44f8b4e20562fcd293aea1d9751f4b5c4e078add
sammylevin10 <sammylevin10@gmail.com>
2020-11-29 16:42:57 -0500

    Working on node and firestore

--- finalproject-backend/app.js
+++ finalproject-backend/app.js
@@ -1,1 +1,17 @@
 // Backend application for Final Project
+const express = require("express");
+
+const app = express();
+const port = 4000;
+
+// Import the routes
+const indexRoute = require("./routes/index.js");
+
+// If someone creates a get request to our root, we send them this string
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+app.use("/", indexRoute);
+
+app.listen(port, () => console.log("Backend is running!"));
--- finalproject-frontend/src/App.js
+++ finalproject-frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Route, BrowserRouter as Router, Redirect } from "react-router-dom";
+import firebase from "firebase/app";
+// import express from "express";
 import "./App.css";
 // Pages
 import ComposePost from "./containers/ComposePost";
@@ -12,6 +14,52 @@ import UserProfile from "./containers/UserProfile";
 import Header from "./components/Header";
 
 function App() {
+  // const router = express.Router();
+  // // Initialize firestore database
+  // const db = firebase.firestore();
+  // // Reference a specific collection
+  // const posts = db.collection("posts");
+
+  // const firebaseConfig = {
+  //   apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  //   authDomain: "trailshare-dynamic-web.firebaseapp.com",
+  //   databaseURL: "https://trailshare-dynamic-web.firebaseio.com",
+  //   projectId: "trailshare-dynamic-web",
+  //   storageBucket: "trailshare-dynamic-web.appspot.com",
+  //   messagingSenderId: "595195663103",
+  //   appId: "1:595195663103:web:f2189a9d41d41d062cb2ca",
+  // };
+
+  // router.get("/:test1", (req, res) => {
+  //   // Get the query parameter from the url and set it to a variable
+  //   const queryId = "test1";
+  //   // Query the collection
+  //   posts
+  //     .doc(queryId) //Look up doc by id
+  //     .get()
+  //     .then(function (doc) {
+  //       if (doc.exists) {
+  //         return res.send(doc.data());
+  //       } else {
+  //         return res.send("No document exists");
+  //       }
+  //     })
+  //     .catch(function (error) {
+  //       return res.send(error);
+  //     });
+  // });
+
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  // useEffect(() => {
+  //   // If there are no firebase apps, initialize the app
+  //   if (!firebase.apps.length) {
+  //     // Initializes firebase
+  //     firebase.initializeApp(firebaseConfig);
+  //   }
+  // }, [firebaseConfig]);
+
   return (
     <div className="App">
       <Header />
